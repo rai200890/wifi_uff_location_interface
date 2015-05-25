7aa02bdb812ae2c451f2e2b9b94cf7d4103aad77
raissaferreira <rai200890@gmail.com>
2015-05-24 23:36:20 -0300

    SNMP get method.

--- app/models/snmp_status.rb
+++ app/models/snmp_status.rb
@@ -1,9 +1,34 @@
 class SnmpStatus
-  include ActiveModel
+  include ActiveModel::Model
 
-  def initialize ap
-   @channel
-   @hostname
+  attr_accessor :host, :community, :protocol, :port, :version, :mib_modules
 
+  VERSIONS = [:SNMPv1, :SNMPv2c]
+
+  def initialize params = {}
+    @host = params[:host]
+    @community = params[:community]
+    @protocol = params[:protocol]
+    @port = params[:port]
+    @version = params[:version]
+    @mib_modules = params[:mib_modules] || ['RFC1213-MIB']
   end
+
+  def get fields = ['RFC1213-MIB::sysDescr.0']
+    SNMP::Manager.open(manager_options) do |manager|
+      begin
+        response = manager.get(fields)
+      rescue Exception => e
+        self.errors.add(:base, e.message)
+        false
+      end
+    end
+  end
+
+  private
+
+  def manager_options
+    {host: host, community: community, version: version, port: port, mib_modules: mib_modules}
+  end
+
 end
