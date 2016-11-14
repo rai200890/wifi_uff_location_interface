4fd000a8d86a468367187d0df4c9e3dfe3263544
Raissa Ferreira <rai200890@gmail.com>
2016-11-14 06:00:40 -0200

    Fix production base url and api url

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 'use strict';
 
   angular.module("wifiUffLocation")
-    .constant('API_URL', "https://wifi-uff-location-api.herokuapp.com/")
-    .constant('BASE_URL', "https://wifi-uff-location.herokuapp.com")
+    .constant('API_URL', "https://wifi-uff-location-api.herokuapp.com")
+    .constant('BASE_URL', "https://wifi-uff-location-interface.herokuapp.com")
     .constant('WHITELISTED_DOMAINS', ["wifi-uff-location-api.herokuapp.com"]);
